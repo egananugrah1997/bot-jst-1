b19864a2b276bee2eaa31b723e7fb555bbe68a64
egananugrah1997 <68815044+egananugrah1997@users.noreply.github.com>
2021-05-29 00:56:56 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(
         msg.chat.id,
         'hello ${msg.chat.first_name}, wellcome..\n
-        click /predict
+        click /predict'
     );   
 });
 
