ee4a9d94fc5ce2f0288fd56b5a42461b58d8a101
egananugrah1997 <68815044+egananugrah1997@users.noreply.github.com>
2021-05-29 00:08:42 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -61,6 +61,7 @@ r.get('/prediction/:i/:r', function(req, res, next) {
             parseFloat(req.params.i), //string to float
             parseFloat(req.params.r)
         ]
+        
     ).then(jres)=>{
         res.json(jres);
     })
