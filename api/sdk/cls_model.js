8ef92e56e08abc94fb676baae029eeeb74a94e65
egananugrah1997 <68815044+egananugrah1997@users.noreply.github.com>
2021-06-04 23:22:50 +0700

    Create cls_model.js

---
+++ api/sdk/cls_model.js
@@ -0,0 +1,47 @@
+const tf = require('@tensorflow/tfjs-node');
+
+function normalized(data){ // i & r
+    i = (data[0] - 12.585) / 6.813882
+    r = (data[1] - 51.4795) / 29.151289
+    v = (data[2] - 650.4795) / 552.635
+    p = (data[3] - 10620.65) / 12152.78
+    return [i, r, v , p]
+}
+
+const argFact = (compareFn) => (array) => array.map((el, idx) => [el, idx]).reduce(compareFn)[1]
+const argMax =  argFact((min, el) => (el[0] > min[0] ? el : min))
+function Argmax (res){
+  label = "NORMAL"
+  if(argmax(res) ==1){
+       label = "OVER VOLTAGE"
+  }if(argmax(res) == 2){
+      label = "Drop VOLTAGE"
+}
+async function classify(data){
+    let in_dim = 4;
+    
+    data = normalized(data);
+    shape = [1, in_dim];
+
+    tf_data = tf.tensor2d(data, shape);
+
+    try{
+        // path load in public access => github
+        const path = 'https://raw.githubusercontent.com/egananugrah1997/bot-jst-1/main/public/ex_model/model.json';
+        const model = await tf.loadGraphModel(path);
+        
+        predict = model.predict(
+                tf_data
+        );
+        result = predict.dataSync();
+        return Argmax ( result );//denormalized( result );
+        
+    }catch(e){
+      console.log(e);
+    }
+}
+
+module.exports = {
+    classify: classify 
+}
+  
